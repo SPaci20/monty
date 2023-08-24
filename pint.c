b1dbfd2d78ed24b67c20a598204f9b3808e62354
Vincidax <vincentdx18@gmail.com>
2023-08-23 17:22:30 -0700

    stderred

--- pint.c
+++ pint.c
@@ -5,6 +5,7 @@
 /**
  * pint - Prints the value at the top of the stack.
  * @stack: Pointer to the stack.
+ * @line_number: Line number in the file.
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
